173126e2cde68a0a8f8905391e8e57ea66bd36b5
Wilhelm <admin@vetrix.dev>
2025-01-24 20:16:44 +0100

    type

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,35 @@
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         // Uncomment this block to pass the first stage
+
+        Set<String> commands = new HashSet<>();
+        commands.add("type");
+        commands.add("exit");
+        commands.add("echo");
         do {
             System.out.print("$ ");
 
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
 
-            String[] input_array = input.split(" ",2);
-            if(input_array[0].toLowerCase().startsWith("exit")) {
+            String[] input_array = input.split(" ", 2);
+
+            if (input_array[0].equalsIgnoreCase("exit")) {
                 System.exit(0);
-            } else if (input_array[0].contains("echo")) {
+            } else if (input_array[0].equalsIgnoreCase("echo")) {
                 System.out.println(input_array[1]);
+            } else if (input_array[0].equalsIgnoreCase("type")) {
+                if (commands.contains(input_array[1])) {
+                    System.out.printf("%s is a shell builtin%n", input_array[1]);
+                } else {
+                    System.out.printf("%s: command not found%n", input_array[1]);
+                }
             } else {
-                System.out.printf("%s: command not found%n", input);
+                System.out.printf("%s: command not found%n", input_array[0]);
             }
         } while (true);
     }
