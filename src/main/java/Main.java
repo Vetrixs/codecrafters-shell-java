23bca169835675da8db323b68a2d7103aa985dfe
Wilhelm <admin@vetrix.dev>
2025-01-24 20:09:16 +0100

    echo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,11 +9,14 @@ public class Main {
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
 
-
-            if(input.toLowerCase().startsWith("exit")) {
+            String[] input_array = input.split(" ",2);
+            if(input_array[0].toLowerCase().startsWith("exit")) {
                 System.exit(0);
+            } else if (input_array[0].contains("echo")) {
+                System.out.println(input_array[1]);
+            } else {
+                System.out.printf("%s: command not found%n", input);
             }
-            System.out.printf("%s: command not found%n", input);
         } while (true);
     }
 }
