0d0314274504e670c962aba90bf841cf64781f79
Wilhelm <admin@vetrix.dev>
2025-01-24 20:00:06 +0100

    loop

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         // Uncomment this block to pass the first stage
-       System.out.print("$ ");
+        do {
+            System.out.print("$ ");
 
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
 
-        System.out.printf("%s: command not found", input);
+            System.out.printf("%s: command not found%n", input);
+        } while (true);
     }
 }
